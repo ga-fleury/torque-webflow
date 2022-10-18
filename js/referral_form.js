249d6313b1fe3769fa88835206177b16ed51ac13
ga-fleury <gabrielfleury95@gmail.com>
2022-10-18 13:46:56 -0300

    referral form validation

--- js/referral_form.js
+++ js/referral_form.js
@@ -1,12 +1,10 @@
-$("referral-submit").css("background", "red");
-
 /**
  * reference to the form element fields
  */
 
 const firstNameField = $("#first-name");
 const lastNameField = $("#last-name");
-const companyEmailField = ("#company-email");
+const companyEmailField = $("#company-email");
 const companyNameField = $("#company-name");
 const phoneNumberField = $("#phone-number");
 const fleetSizeField = $("#fleet-size");
@@ -24,39 +22,59 @@ const commentsField = $("#comments");
 
 const submitButton = $("#referral-submit");
 
+/**
+ * Regular expressions for each type of validation
+ */
 
-//------------------------------- VALIDATION FUNCTIONS---------------------------------
+const phoneRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+const zipCodeRegex = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-function validatePhone(field) {
-    let isValidPhone = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
-    if (field.val().match(isValidPhone)) {
-        if (phoneErrorMessageDisplayed == true) {
-            $("#error-phone").detach();
-            phoneErrorMessageDisplayed = false;
-        }
-    } else {
-        if (phoneErrorMessageDisplayed == false) {
-            field.next(
-                "<p id='error-phone' style='color: aliceblue;'>Please enter a valid value.</p>"
-            );
-            phoneErrorMessageDisplayed = true;
-        }
+//------------------------------- VALIDATION FUNCTION START---------------------------------
+
+function validFieldCheck(field, validationRegex) {
+    if (field.val().match(validationRegex) || field.val().length == 0) {
+        $(`#error-${field.attr("id")}`).detach();
+    } else if (
+        !field.val().match(validationRegex) &&
+        !$(`#error-${field.attr("id")}`).length
+    ) {
+        field.after(
+            `<p id='error-${field.attr(
+                "id"
+            )}' style='color: red;'>Please enter a valid value.</p>`
+        );
     }
 }
 
-function validateEmail(field) {
-    let isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (field.val().match(isValidEmail)) {
-        if (emailErrorMessageDisplayed == true) {
-            $("#error-email").detach();
-            emailErrorMessageDisplayed = false;
-        }
-    } else {
-        if (emailErrorMessageDisplayed == false) {
-            field.next(
-                "<p id='error-email' style='color: aliceblue;'>Please enter a valid value.</p>"
-            );
-            emailErrorMessageDisplayed = true;
-        }
-    }
+//------------------------------- VALIDATION FUNCTION END---------------------------------
+
+/**
+ * attributing each field to its correct validation function
+ */
+
+ phoneNumberField.keyup(function () {
+    validFieldCheck(phoneNumberField, phoneRegex);
+});
+
+companyEmailField.keyup(function () {
+    validFieldCheck(companyEmailField, emailRegex)
+});
+
+referralCompanyEmailField.keyup(function () {
+    validFieldCheck(referralCompanyEmailField, emailRegex)
+});
+
+zipCodeField.keyup(function () {
+    validFieldCheck(zipCodeField, zipCodeRegex)
+});
+
+//------------------------------- SUBMIT FUNCTION ---------------------------------
+
+/**
+ //TODO        make referral submission fuction
+ */
+
+function referralSubmit() {
+
 }
