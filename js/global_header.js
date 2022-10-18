1cc477f0ffa692487341520532da18e5847c246a
ga-fleury <gabrielfleury95@gmail.com>
2022-10-18 11:55:36 -0300

    header fixes

--- js/global_header.js
+++ js/global_header.js
@@ -11,23 +11,53 @@ navMenuButtons.on("click", function () {
     }
 });
 
-const navLinks = $("#contact-over, #contact-over2,  #about-over, #about-over2");
+const navLinks = $(
+    "#contact-over, #contact-over2, #about-over, #about-over2, #getstarted"
+);
 
 navLinks.on("click", function () {
     $("body").removeClass("overflow-hidden");
     menuOpen = false;
 });
 
+/* the viewport is less than 991 pixels wide */
+
 $("#login1").on("click", function () {
-    $('#button1').triggerHandler('tap');
-    menuOpen = false;
+    if (!window.matchMedia("(min-width: 991px)").matches) {
+        $("#button1").triggerHandler("tap");
+        $("body").removeClass("overflow-hidden");
+        menuOpen = false;
+    }
+});
+
+/*  */
+$(window).on("resize", function () {
+    if (menuOpen) {
+        $("#navshadow").addClass("navshadow-display");
+        console.log("menu opened");
+    } else {
+        $("#navshadow").removeClass("navshadow-display");
+        console.log("menu closed");
+    }
+});
+
+$(window).on("orientationchange", function () {
+    if (menuOpen) {
+        $("#navshadow").css(["display", "block", "opacity", "1"]);
+        console.log("menu opened");
+    } else {
+        $("#navshadow").removeClass("navshadow-display");
+        console.log("menu closed");
+    }
 });
 
 $("#login2").on("click", function () {
-    $('#button2').triggerHandler('tap');
+    $("#button2").triggerHandler("tap");
+    $("body").removeClass("overflow-hidden");
     menuOpen = false;
 });
+
 $("#login3").on("click", function () {
-    $('#button2').triggerHandler('tap');
+    $("#button2").triggerHandler("tap");
     menuOpen = false;
 });
