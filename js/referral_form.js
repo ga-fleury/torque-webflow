bf32c8f8b85fabcb9230f3e293f00706ca2f396c
ga-fleury <gabrielfleury95@gmail.com>
2022-10-19 08:17:41 -0300

    before BA - Gabriel

--- js/referral_form.js
+++ js/referral_form.js
@@ -1,3 +1,6 @@
+//TODO: REPLACE FORM WRAPPER ID WITH CORRECT FORM WRAPPER ID
+//TODO: REPLACE THANK YOU MESSAGE CLASS WITH CORRECT CLASS
+
 /**
  * reference to the form element fields
  */
@@ -21,6 +24,8 @@ const referralCompanyEmailField = $("#referral-company-email");
 const commentsField = $("#comments");
 
 const submitButton = $("#referral-submit");
+const referralForm = $("#referral-form");
+const referralFormWrapper = $("#referral-form-wrapper");
 
 /**
  * Regular expressions for each type of validation
@@ -28,7 +33,8 @@ const submitButton = $("#referral-submit");
 
 const phoneRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
 const zipCodeRegex = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const SAPRegex = /^[0-9]{1,128}$/;
 
 //------------------------------- VALIDATION FUNCTION START---------------------------------
 
@@ -40,9 +46,9 @@ function validFieldCheck(field, validationRegex) {
         !$(`#error-${field.attr("id")}`).length
     ) {
         field.after(
-            `<p id='error-${field.attr(
-                "id"
-            )}' style='color: red;'>Please enter a valid value.</p>`
+            `<p id='error-${field.attr("id")}' style='color: red;'>
+            Please enter a valid value.
+            </p>`
         );
     }
 }
@@ -53,20 +59,27 @@ function validFieldCheck(field, validationRegex) {
  * attributing each field to its correct validation function
  */
 
- phoneNumberField.keyup(function () {
+phoneNumberField.keyup(function () {
     validFieldCheck(phoneNumberField, phoneRegex);
 });
 
 companyEmailField.keyup(function () {
-    validFieldCheck(companyEmailField, emailRegex)
+    validFieldCheck(companyEmailField, emailRegex);
 });
 
 referralCompanyEmailField.keyup(function () {
-    validFieldCheck(referralCompanyEmailField, emailRegex)
+    validFieldCheck(referralCompanyEmailField, emailRegex);
 });
 
 zipCodeField.keyup(function () {
-    validFieldCheck(zipCodeField, zipCodeRegex)
+    validFieldCheck(zipCodeField, zipCodeRegex);
+});
+
+submitButton.on("click", function () {
+    referralSubmit();
+    referralForm.detach()
+    referralFormWrapper.append( "<h2 class='thank-you-title'>Thank you for submitting your refferal</h2><br><p class='thank-you-sub'>some other message here</p>" )
+    return false;
 });
 
 //------------------------------- SUBMIT FUNCTION ---------------------------------
@@ -75,6 +88,18 @@ zipCodeField.keyup(function () {
  //TODO        make referral submission fuction
  */
 
+const pageUTMs = window.location.search;
 function referralSubmit() {
+    const serialize = $("form").serialize();
+    const UTM_URL = serialize.concat(`&${pageUTMs.replace("?", "")}`);
+    const finalQuery = UTM_URL.concat(
+        "&Campaign_ID__c=7018B000000Hw6iQAC&GCLID__c=testing_gclid"
+    );
+    const URL =
+        "https://test.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&debug=1&debugEmail=mobilemaintenance@doublenines.co&oid=00D8B0000008hPZ&";
+    const finalURL = URL.concat(finalQuery);
 
+    fetch(finalURL, {
+        method: "POST",
+    });
 }
